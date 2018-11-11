02a91ff9634f0410d4c8d8d896c78069a5a37570
sivvig <vignesh.siva@gmail.com>
2018-11-11 12:52:11 -0500

    Added pkmn typings to table view

--- src/main/java/com/pdl/prepTool/Tool.java
+++ src/main/java/com/pdl/prepTool/Tool.java
@@ -21,19 +21,32 @@ import java.util.Scanner;
 public class Tool {
     public static void main(String[] args) throws IOException {
         String FILE_PATH;
+
+
+        //Production
         String DIRECTORY = System.getenv("APP_HOME");
+
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter Pokemon List file:");
+
+        //For testing
+        //FILE_PATH = args[0];
+
+        //Production
         FILE_PATH = sc.nextLine();
 
-        //String FILE_PATH = args[0];
 
         PokeApi pokeApi = new PokeApiClient();
 
+        //For testing
+        //List<String> pkmnList = Files.readAllLines(new File(FILE_PATH).toPath(), Charsets.UTF_8);
+
+        //Production
         List<String> pkmnList = Files.readAllLines(new File(DIRECTORY, FILE_PATH).toPath(), Charsets.UTF_8);
+
         ArrayList<Integer> pkmnIndex = new ArrayList<>();
         ArrayList<Pokemon> party = new ArrayList<>();
-        String[] headers = {"Name", "HP", "Attack", "Defense", "Sp.Attack", "Sp.Defense", "Speed"};
+        String[] headers = {"Name", "Type", "HP", "Attack", "Defense", "Sp.Attack", "Sp.Defense", "Speed"};
         String[][] data = new String[pkmnList.size()][7];
 
         for(int i=0; i<pkmnList.size(); i++){
@@ -47,28 +60,33 @@ public class Tool {
 
             Pokemon activePkmn = party.get(i);
 
-            //List<PokemonType> activeTypes = activePkmn.getTypes();
+            List<PokemonType> activeTypes = activePkmn.getTypes();
             List<PokemonStat> activePkStats = activePkmn.getStats();
 
             String name = activePkmn.getName().substring(0, 1).toUpperCase() + activePkmn.getName().substring(1);
 
-            /*int hp = activePkStats.get(5).component3();
-            int attack = activePkStats.get(4).component3();
-            int defense = activePkStats.get(3).component3();
-            int spAtk = activePkStats.get(2).component3();
-            int spDef = activePkStats.get(1).component3();
-            int speed = activePkStats.get(0).component3();*/
+            String type1;
+            String type2;
+            String type;
+            //Example:
+            //int hp = activePkStats.get(5).component3();
 
-            /*System.out.println("Name: " + name);
-            System.out.println("HP: " + hp);
-            System.out.println("Attack: " + attack);
-            System.out.println("Defense: " + defense);
-            System.out.println("Sp. Attack: " + spAtk);
-            System.out.println("Sp. Defense: " + spDef);
-            System.out.println("Speed: " + speed);
-            System.out.println();*/
+            if(activeTypes.size()>1) {
+                type1 = activeTypes.get(1).component2().component1().substring(0, 1).toUpperCase() + activeTypes.get(1).component2().component1().substring(1);
+                type2 = activeTypes.get(0).component2().component1().substring(0,1).toUpperCase() + activeTypes.get(0).component2().component1().substring(1);
 
-            //System.out.println(activeTypes);
+                type = type1 + "/" + type2;
+
+
+                //Typing id
+                //System.out.println(activeTypes.get(0).component2().component3());
+
+            } else {
+                type1 = activeTypes.get(0).component2().component1().substring(0,1).toUpperCase() + activeTypes.get(0).component2().component1().substring(1);
+                type2 = "";
+
+                type = type1 + type2;
+            }
 
             String hp = String.valueOf(activePkStats.get(5).component3());
             String attack = String.valueOf(activePkStats.get(4).component3());
@@ -77,11 +95,12 @@ public class Tool {
             String spDef = String.valueOf(activePkStats.get(1).component3());
             String speed = String.valueOf(activePkStats.get(0).component3());
 
-            String[] rowData = {name, hp, attack, defense, spAtk, spDef, speed};
+            String[] rowData = {name, type, hp, attack, defense, spAtk, spDef, speed};
             data[i] = rowData;
         }
 
         System.out.println(FlipTable.of(headers,data));
+
         System.out.println("\nPress ENTER to close.\n");
         sc.nextLine();
     }
